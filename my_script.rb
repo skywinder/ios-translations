f72c270ef016ee8d6cc1762ef3787ee518821ae2
Petr Korolev <sky4winder@gmail.com>
2015-12-17 17:07:57 +0200

    cm

--- my_script.rb
+++ my_script.rb
@@ -1,6 +1,8 @@
 class Translator
   @dir = ""
   @translation = ""
+  @tr_langs = []
+  @tr_words = []
 
   def initialize(dir, translations)
     @dir = dir
@@ -8,10 +10,10 @@ class Translator
   end
 
   def run
-    puts self.inspect
     puts get_trans_files
     puts existing_trans_langs
-
+    puts parse_translation
+    puts self.inspect
   end
 
   def get_trans_files
@@ -19,8 +21,21 @@ class Translator
     Dir[string]
     # "/path/to/search/**/*.rb"
   end
+
   def existing_trans_langs
-    get_trans_files.map{|f| File.basename(f, ".xliff")}
+    get_trans_files.map { |f| File.basename(f, ".xliff") }
+  end
+
+  def parse_translation
+    line_num = 0
+    File.open(@translation).each do |line|
+      line_split = line.split(",")
+      if line_num == 0
+        @tr_langs = line_split
+      else
+        @tr_words.push(line_split)
+      end
+    end
   end
 end
 
