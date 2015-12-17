88bef8cc1973771ee86858f4bd95825d42ca52e8
Petr Korolev <sky4winder@gmail.com>
2015-12-17 16:46:16 +0200

    create classs

--- my_script.rb
+++ my_script.rb
@@ -1,3 +1,19 @@
-
 class Translator
+  @dir = ""
+  @translation = ""
+
+  def initialize(dir, translations)
+    @dir = dir
+    @translation = translations
+  end
+
+  def run
+    puts self.inspect
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  translator = Translator.new("dir_path", "langs_file")
+  translator.run
 end
